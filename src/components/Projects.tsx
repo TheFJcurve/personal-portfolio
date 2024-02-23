108af85380fc613f01055e0ac383b871327d76bd
Sargun <sargunbhatti27@gmail.com>
2024-02-23 03:26:49 -0500

    added projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,35 @@
 import { Divider, Heading } from "@chakra-ui/react";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
     <>
       <Heading size={"md"}>Projects</Heading>
       <Divider margin={2} />
+      <ProjectCard
+        orientation="left"
+        projectTitle={"Resume Generator"}
+        projectDescription={[
+          "Tired of writing resumes? Generate a resume in minutes!",
+          "Created using MERN Stack, older resumes can be re-used and edited.",
+          "One component can be shared across multiple resumes.",
+          "LaTeX to generate beautiful and stable resumes in a choice of fonts.",
+        ]}
+        projectLinks={[
+          "https://github.com/TheFJcurve/resume-generator-app",
+          "https://github.com/TheFJcurve/Resume-Generator",
+        ]}
+        projectLinkDisplayName={["GitHub", "Bash Version"]}
+        skills={[
+          "ReactJS",
+          "NodeJS",
+          "ExpressJS",
+          "MongoDB",
+          "ChakraUI",
+          "LaTeX",
+          "Bash",
+        ]}
+      />
     </>
   );
 };
